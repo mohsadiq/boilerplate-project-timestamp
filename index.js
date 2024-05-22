394f9aad9c570b565ca803a0f77cbb1b52b8e1f5
Sadiq Hosainy <moh.sadiq22@gmail.com>
2024-05-22 14:52:47 +0100

    commit

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?",(req, res)=>{
+  let input = req.params.date;
+});
 
 
 // Listen on port set in environment variable or default to 3000
